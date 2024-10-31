c66f3c958c25a450e5d45c18f3de9de2ac52b5e9
NevolinM <newolin208@gmail.com>
2024-10-31 10:19:06 +0300

    Update task 2.py

--- Лабораторная работа №2/task 2.py
+++ Лабораторная работа №2/task 2.py
@@ -14,3 +14,17 @@ while True:
     spend += spend * increase
 money_capital = int(abs(money_capital))
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
+
+
+!!!Исправленный вариант ниже !!!
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+money_capital = 0
+
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+for i in range(0, months):
+    money_capital += salary - spend
+    spend += spend * increase
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", int(abs(money_capital)))
